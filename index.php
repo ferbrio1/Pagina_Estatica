4a406fe6a3e08c92bbe42ee4af41104c42e5580b
ferbrio1 <146587541+ferbrio1@users.noreply.github.com>
2023-09-30 13:45:47 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Información sobre Ansible y Angular</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            color: #333;
+            background-color: #f0f0f0;
+            margin: 20px;
+            max-width: 800px;
+            margin: auto;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+        }
+
+        h1 {
+            color: #0056b3;
+        }
+
+        p {
+            line-height: 1.6;
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        li {
+            margin-bottom: 10px;
+        }
+
+        a {
+            text-decoration: none;
+            color: #007bff;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <h1>¿Qué es Ansible?</h1>
+    <p>Ansible es una plataforma de automatización de TI de código abierto que facilita la automatización de tareas complejas de configuración, implementación y administración de sistemas. Utiliza un lenguaje de configuración simple basado en YAML y no requiere agentes en los sistemas remotos.</p>
+
+    <h1>Características principales de Ansible</h1>
+    <ul>
+        <li>Automatización sin agentes</li>
+        <li>Infraestructura como código (IaC)</li>
+        <li>Orquestación de múltiples servidores</li>
+        <li>Escalabilidad y flexibilidad</li>
+        <li>Integración con herramientas de gestión de configuración</li>
+    </ul>
+
+    <h1>¿Qué es Angular?</h1>
+    <p>Angular es un framework de desarrollo para construir aplicaciones web de una sola página (SPA) y dinámicas. Está desarrollado y mantenido por Google y una comunidad de desarrolladores individuales y corporativos.</p>
+
+    <h1>Características principales de Angular</h1>
+    <ul>
+        <li>Desarrollo basado en componentes</li>
+        <li>Enlace bidireccional de datos</li>
+        <li>Routing para construir aplicaciones de una sola página</li>
+        <li>Manejo de formularios y validación</li>
+        <li>Amplia comunidad y soporte</li>
+    </ul>
+
+    <h1>Beneficios de usar Ansible y Angular</h1>
+    <ul>
+        <li>Simplicidad y facilidad de uso</li>
+        <li>Ahorro de tiempo y recursos</li>
+        <li>Consistencia en la configuración</li>
+        <li>Gestión centralizada</li>
+        <li>Desarrollo rápido y escalable de aplicaciones web</li>
+    </ul>
+
+    <h1>Recursos Adicionales</h1>
+    <p>Para obtener más información sobre Ansible y Angular, puedes consultar los siguientes recursos:</p>
+    <ul>
+        <li><a href="https://www.ansible.com/">Sitio web oficial de Ansible</a></li>
+        <li><a href="https://docs.ansible.com/">Documentación de Ansible</a></li>
+        <li><a href="https://github.com/ansible/ansible">Repositorio de GitHub de Ansible</a></li>
+        <li><a href="https://angular.io/">Sitio web oficial de Angular</a></li>
+        <li><a href="https://angular.io/docs">Documentación de Angular</a></li>
+        <li><a href="https://github.com/angular/angular">Repositorio de GitHub de Angular</a></li>
+    </ul>
+</body>
+</html>
